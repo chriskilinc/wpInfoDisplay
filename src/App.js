5e82107ae640b5e6ff96792ac8a493a147e2cf02
chriskilinc <chriskilinc@gmail.com>
2018-04-19 13:40:36 +0200

    Added function getArticles() to fetch from wordpress api using Axios. State will update if fetch returns anything

--- src/App.js
+++ src/App.js
@@ -1,19 +1,51 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+
+    //  Initial State
+    this.state = {
+      articles: [],
+      wpApiUrl: "http://smartgallery.chriskilinc.com/wp-json/wp/v2/posts?_embed"
+    }
+  }
+
+  componentDidMount = () => {
+    //  Get Articles when component have mounted
+    //  getArticles() will update state if successfull
+    this.getArticles(this.state.wpApiUrl);
+  }
+
+  // Async Api Call using Axios to talk with Wordpress Api
+  getArticles = (url) => {
+    axios
+      .get(url)
+      .then(response => {
+        this.setState({
+          articles: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    return ( 
+      this.state.articles.length > 0 ?
+      <div>
+        <p>successfull</p>
+      </div>
+      :
+      <div className="application__loading">
+        <p>Loading.. <br /><i>Fetching from api</i></p>
       </div>
     );
   }
