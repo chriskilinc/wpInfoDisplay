e080a570e56f3946135e61204327bd20f450e913
chriskilinc <chriskilinc@gmail.com>
2018-05-06 13:07:01 +0200

    Minor css classes added and some height styling

--- src/components/ArticleAside.js
+++ src/components/ArticleAside.js
@@ -13,15 +13,26 @@ class ArticleAside extends React.Component {
       <section className="article-aside">
         <div className="article-aside--container">
           <article className="article-aside--article">
-            <h1>Article</h1>
+            <div className="article-aside--article--container">
+              <h1>Article</h1>
+            </div>            
           </article>
           <aside className="article-aside--aside">
-            <i>aside</i>
+            <div className="article-aside--aside--container">
+              <div className="article-aside--clock">
+                <h1>00:00</h1>
+                <p>04 Maj</p>
+              </div>
+              <div className="article-aside--aside-article">
+                <h1>Title</h1>
+                <p>Contents</p>
+              </div>
+            </div>
           </aside>
         </div>
         <footer className="article-aside--footer">
           <div className="article-aside--footer--banner">
-            <p>footer</p>
+            <img src="http://touch.tvostra.se/wp-content/uploads/2018/04/ostra_touch_topnav-1.png"/>
           </div>
         </footer>
       </section>
