3b1da8df83bf44c245bf504b0822732128fb74af
chriskilinc <chriskilinc@gmail.com>
2018-05-18 12:44:27 +0200

    Added "hasAside" to settings in config.js

--- src/config.js
+++ src/config.js
@@ -3,8 +3,9 @@ module.exports = {
   wpApiUrl: "http://smartgallery.chriskilinc.com/wp-json/wp/v2/posts?_embed",  //  Wordpress REST API url
   applicationName: "SmartGallery",   // Application name, this will also be the browser tab title
   settings: {
-    cycleInSeconds: 2,  //  The amount of time an item will show before changing to the next in que
+    cycleInSeconds: 7.5,  //  The amount of time an item will show before changing to the next in que
     totalCycles: 1,       //  Total Number of cycles the gallery will run before fetching the Api
+    hasAside: false,
   },
   placeholder: {
     image: "http://sgy.chriskilinc.com/wp-content/uploads/2018/05/black-1.png",   //  Placeholder Image for articles without an FeaturedImage attatched
